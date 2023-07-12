6b967fce7a21da040dd918a132fb8de02d9cc025
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-07-12 12:11:07 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,88 +1,67 @@
-import torch
+mport torch
+import setka
+import torchvision.datasets
 import setka.base
 import setka.pipes
-import sklearn.datasets
+import os
 import numpy
+import sys
 
-name = 'iris_test'
+sys.path.append(os.getcwd())
 
-class Iris(setka.base.Dataset):
-    def __init__(self, valid_split=0.1, test_split=0.1):
-        super()
-        data = sklearn.datasets.load_iris()
+from datasets import MNIST
+from models import ThreeLayersFullyConnected, LeNetFullyConvolutional
 
-        X = data['data']
-        y = data['target']
+ds = MNIST()
 
-        n_valid = int(y.size * valid_split)
-        n_test =  int(y.size * test_split)
+## Trying fully connected
 
-        order = numpy.random.permutation(y.size)
+net = ThreeLayersFullyConnected(n = [100, 100, 10])
 
-        X = X[order, :]
-        y = y[order]
+def loss(predictions, targets):
+    return torch.nn.functional.cross_entropy(
+        predictions[0], targets[0])
 
-        self.data = {
-            'valid': X[:n_valid, :],
-            'test': X[n_valid:n_valid + n_test, :],
-            'train': X[n_valid + n_test:, :]
-        }
+def acc(predictions, targets):
+    return (predictions[0].argmax(dim=-1) == targets[0]).sum(), targets[0].numel()
 
-        self.targets = {
-            'valid': y[:n_valid],
-            'test': y[n_valid:n_valid + n_test],
-            'train': y[n_valid + n_test:]
-        }
+trainer = setka.base.Trainer(
+    net,
+    criterion=loss,
+    optimizers=[setka.base.OptimizerSwitch(net, torch.optim.Adam, lr=3.0e-4)],
+    pipes=[setka.pipes.ComputeMetrics([loss, acc]),
+               setka.pipes.ExponentialWeightAveraging(epoch_start=2)]
+)
 
-    def getlen(self, subset):
-        return len(self.targets[subset])
+for index in range(5):
+    trainer.train_one_epoch(ds, subset='train', batch_size=128)
+    trainer.validate_one_epoch(ds, subset='train', batch_size=128)
+    trainer.validate_one_epoch(ds, subset='valid', batch_size=128)
 
-    def getitem(self, subset, index):
-        features = torch.Tensor(self.data[subset][index])
-        class_id = torch.Tensor(self.targets[subset][index:index+1])
-        return [features], [class_id], subset + "_" + str(index)
+## Trying fully connected
 
+net = ThreeLayersFullyConnected(n = [100, 100, 10], batch_norms=True)
 
-class IrisNet(setka.base.Network):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = torch.nn.Linear(4, 100)
-        self.fc2 = torch.nn.Linear(100, 3)
+def loss(predictions, targets):
+    return torch.nn.functional.cross_entropy(
+        predictions[0], targets[0])
 
-    def forward(self, x):
-        return [self.fc2(self.fc1(x[0]))]
+def acc(predictions, targets):
+    return (predictions[0].argmax(dim=-1) == targets[0]).sum(), targets[0].numel()
 
+trainer = setka.base.Trainer(
+    net,
+    criterion=loss,
+    optimizers=[setka.base.OptimizerSwitch(net, torch.optim.Adam, lr=3.0e-4)],
+    pipes=[setka.pipes.ComputeMetrics([loss, acc]),
+               setka.pipes.ExponentialWeightAveraging(epoch_start=2)]
+)
 
-ds = Iris()
-model = IrisNet()
+for index in range(5):
+    trainer.train_one_epoch(ds, subset='train', batch_size=128)
+    trainer.validate_one_epoch(ds, subset='train', batch_size=128)
+    trainer.validate_one_epoch(ds, subset='valid', batch_size=128)
 
-
-def loss(pred, targ):
-    return torch.nn.functional.cross_entropy(pred[0], targ[0][:, 0].long())
-
-
-def accuracy(pred, targ):
-    predicted = pred[0].argmax(dim=1)
-    # print(predicted.size())
-    # print(targ[0].size())
-    return (predicted == targ[0][:, 0].long()).sum(), predicted.numel()
-
-
-trainer = setka.base.Trainer(model,
-                             optimizers=[setka.base.OptimizerSwitch(model, torch.optim.Adam, lr=3.0e-3)],
-                             criterion=loss,
-                             pipes=[
-                                setka.pipes.ComputeMetrics(metrics=[loss, accuracy]),
-                                setka.pipes.ReduceLROnPlateau(metric='loss'),
-                                setka.pipes.ExponentialWeightAveraging(),
-                                setka.pipes.WriteToTensorboard(name=name),
-                                setka.pipes.Logger(name=name)
-                              ])
-
-for index in range(100):
-    trainer.train_one_epoch(ds, subset='train')
-    trainer.validate_one_epoch(ds, subset='train')
-    trainer.validate_one_epoch(ds, subset='valid')
 
 # assert(trainer._metrics['valid']['accuracy'] > 0.9)
 print(f"\n\n\nScore: {trainer._metrics['valid']['accuracy']}\n\n\n")
