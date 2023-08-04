5024924f1717754545c982dc26d87265b1c10b43
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-08-03 23:21:04 -0400

    Update example.py

--- example.py
+++ example.py
@@ -13,12 +13,12 @@ class MNIST(setka.base.Dataset):
             # torchvision.transforms.RandomCrop(32, padding=4),
             # torchvision.transforms.RandomHorizontalFlip(),
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            torchvision.transforms.Normalize((0.5), (0.5)),
         ])
 
         test_transforms = torchvision.transforms.Compose([
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            torchvision.transforms.Normalize((0.5), (0.5)),
         ])
 
         self.train_data = torchvision.datasets.MNIST(
